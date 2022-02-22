2a6748112abaa5e6e859fd7d35c844a3255221b6
Mandy Yao <mandyyao@Mandys-MacBook-Pro-2.local>
2022-02-21 20:13:17 -0500

    fix unit test

--- test_inputParser.py
+++ test_inputParser.py
@@ -6,12 +6,12 @@ def test_sample() -> None:
 
 def test_validate_json_with_valide_file() -> None:
     validJson = input_data_parser.validate_json('./unitTests/sample_patient1.json')
-    assert validJson == True
+    assert validJson[0] == True
 
 def test_validate_json_with_invalide_file() -> None:
     validJson = input_data_parser.validate_json('./unitTests/sample_fail_file.json')
-    assert validJson == False
+    assert validJson[0] == False
 
 def test_validate_json_with_invalide_file_two() -> None:
     validJson = input_data_parser.validate_json('./unitTests/sample.txt')
-    assert validJson == False
+    assert validJson[0] == False
