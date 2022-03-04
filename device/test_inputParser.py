7ced99d370027dd406f8025e98e9f3119e9dca85
myaoo18 <mandyyao118@gmail.com>
2022-03-04 15:24:24 -0800

    fix test by changing data location

--- device/test_inputParser.py
+++ device/test_inputParser.py
@@ -5,13 +5,13 @@ def test_sample() -> None:
     pass
 
 def test_validate_json_with_valide_file() -> None:
-    validJson = input_data_parser.validate_json('./unitTests/sample_patient1.json')
+    validJson = input_data_parser.validate_json('./data/sample_patient1.json')
     assert validJson[0] == True
 
 def test_validate_json_with_invalide_file() -> None:
-    validJson = input_data_parser.validate_json('./unitTests/sample_fail_file.json')
+    validJson = input_data_parser.validate_json('./data/sample_fail_file.json')
     assert validJson[0] == False
 
 def test_validate_json_with_invalide_file_two() -> None:
-    validJson = input_data_parser.validate_json('./unitTests/sample.txt')
+    validJson = input_data_parser.validate_json('./data/sample.txt')
     assert validJson[0] == False
