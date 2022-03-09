4c493cc0a2852125188044630320a04c650102d8
myaoo18 <mandyyao118@gmail.com>
2022-03-09 13:57:04 -0800

    test unit test

--- device/test_inputParser.py
+++ device/test_inputParser.py
@@ -1,12 +1,15 @@
 import pytest
 import input_data_parser
 
-# def test_validate_json_with_valide_file() -> None:
-#     validJson = input_data_parser.write_to_database('./data/sample_patient1.json')
-#     assert validJson[0] == True
+def test_sample() -> None:
+    pass
+
+def test_validate_json_with_valide_file() -> None:
+    validJson = input_data_parser.write_to_database('../device/data/sample_patient1.json')
+    assert validJson[0] == True
 
 def test_validate_json_with_invalide_file() -> None:
-    validJson = input_data_parser.write_to_database('./data/sample_patient1.json')
+    validJson = input_data_parser.write_to_database('./data/sample_fail_file.json')
     assert validJson[0] == False
 
 def test_validate_json_with_invalide_file_two() -> None:
