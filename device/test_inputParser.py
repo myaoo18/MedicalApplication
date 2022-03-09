dac2c09e50ca068d56b340ecc5947adddcd5599b
myaoo18 <mandyyao118@gmail.com>
2022-03-09 13:49:13 -0800

    add happy path test

--- device/test_inputParser.py
+++ device/test_inputParser.py
@@ -4,9 +4,9 @@ import input_data_parser
 def test_sample() -> None:
     pass
 
-# def test_validate_json_with_valide_file() -> None:
-#     validJson = input_data_parser.write_to_database('./data/sample_patient1.json')
-#     assert validJson[0] == True
+def test_validate_json_with_valide_file() -> None:
+    validJson = input_data_parser.write_to_database('./data/sample_patient1.json')
+    assert validJson[0] == True
 
 def test_validate_json_with_invalide_file() -> None:
     validJson = input_data_parser.write_to_database('./data/sample_fail_file.json')
